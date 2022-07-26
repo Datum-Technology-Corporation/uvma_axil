253956b1eb3edfff4468e7f4fd232b9f58ef35e6
datum-dpoulin <dpoulin@datumtc.ca>
2022-07-26 16:51:01 -0400

    updated regression scripts for new CLI

--- dv/uvmt_axil_st/bin/dev_weekly_regression.sh
+++ dv/uvmt_axil_st/bin/dev_weekly_regression.sh
@@ -1,16 +1,14 @@
 #! /bin/bash
 ########################################################################################################################
-## Copyright 2021 Datum Technology Corporation
+## Copyright 2021-2022 Datum Technology Corporation
 ## SPDX-License-Identifier: Apache-2.0 WITH SHL-2.1
 ########################################################################################################################
 
 
 # Launched from uvma_axil project sim dir
-shopt -s expand_aliases
-source ~/.bashrc
-mio cpel    uvmt_axil_st
-mio sim     uvmt_axil_st -t reads      -s 1 -c
-mio sim     uvmt_axil_st -t writes     -s 1 -c
-mio sim     uvmt_axil_st -t all_access -s 1 -c
+mio sim     uvmt_axil_st -CE
+mio sim     uvmt_axil_st -S -t reads      -s 1 -c
+mio sim     uvmt_axil_st -S -t writes     -s 1 -c
+mio sim     uvmt_axil_st -S -t all_access -s 1 -c
 mio results uvmt_axil_st results
 mio cov     uvmt_axil_st
